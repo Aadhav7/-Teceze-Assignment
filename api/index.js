725e470781e042d6e5055d8d05b7531e5397797d
Jeganathan Digevan <digee12@gmail.com>
2026-06-20 22:36:51 +0530

    Fix Vercel routing and patch debug handler

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ try {
   app = require('../backend/app');
 } catch (error) {
   app = require('express')();
-  app.all('*', (req, res) => {
+  app.all('/(.*)', (req, res) => {
     res.status(500).json({
       message: 'Failed to boot backend app',
       error: error.message,
